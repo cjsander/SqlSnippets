df9728e963b82096c6a1f8adcafab95e34cb5dde
cjsander <chrystalsander@hotmail.com>
2021-10-21 12:28:29 -0500

    Update CommonSql.sql

--- SqlServer/CommonSql.sql
+++ SqlServer/CommonSql.sql
@@ -1,3 +1,10 @@
 select * from sys.schemas
 
 select * from INFORMATION_SCHEMA.TABLES
+
+--get row counts
+SELECT SCHEMA_NAME(a.schema_id), a.name, sum(b.rows)
+FROM sys.objects a
+INNER JOIN sys.partitions b ON a.object_id = b.object_id
+WHERE a.type = 'U'
+GROUP BY a.schema_id, a.name
