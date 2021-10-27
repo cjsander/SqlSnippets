ce2a8c3d09aa4b4e4af1303b1228b8fff4b02500
cjsander <chrystalsander@hotmail.com>
2021-10-27 13:17:34 -0500

    Update CommonSql.sql

--- SqlServer/CommonSql.sql
+++ SqlServer/CommonSql.sql
@@ -2,12 +2,13 @@ select * from sys.schemas
 
 select * from INFORMATION_SCHEMA.TABLES
 
---get row counts
+--get rowcounts
 SELECT SCHEMA_NAME(a.schema_id), a.name, sum(b.rows)
 FROM sys.objects a
 INNER JOIN sys.partitions b ON a.object_id = b.object_id
-WHERE a.type = 'U'
+WHERE a.type = 'U'   
 GROUP BY a.schema_id, a.name
+HAVING sum(b.rows) > 0
 
 --find column name in tables
 select distinct TABLE_SCHEMA, TABLE_NAME, COLUMN_NAME
